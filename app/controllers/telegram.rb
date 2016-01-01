55d12d3fa9fcba98fb3100acfcca5ca0ee3a5990
Shved <snuggleshved@gmail.com>
2016-01-01 23:38:31 +0300

    Add Test

--- app/controllers/telegram.rb
+++ app/controllers/telegram.rb
@@ -5,6 +5,7 @@ Telegram::Bot::Client.run(token) do |bot|
     case message.text
     when 'start'
       bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      offset = update_id of last processed update + 1
     end
   end
 end
