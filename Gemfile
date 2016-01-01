e8862cc62cfacd9dd5fe31304187bf45f31a4c4e
Shved <snuggleshved@gmail.com>
2016-01-02 00:03:21 +0300

    Like Chinese We copy and paste

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'telegram-bot-ruby'
+gem 'telegram_bot'
 gem 'foreman'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- app/controllers/telegram.rb
+++ app/controllers/telegram.rb
@@ -1,11 +1,24 @@
-require 'telegram/bot'
-token = '149230034:AAH_Q-s-KOxWaafJwlOMDWYQLxDMHNCehVU'
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when 'start'
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      offset = update_id of last processed update + 1
+require 'telegram_bot'
+require 'pp'
+require 'logger'
+
+logger = Logger.new(STDOUT, Logger::DEBUG)
+
+bot = TelegramBot.new(token: '149230034:AAH_Q-s-KOxWaafJwlOMDWYQLxDMHNCehVU', logger: logger)
+logger.debug "starting telegram bot"
+
+bot.get_updates(fail_silently: true) do |message|
+  logger.info "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /greet/i
+      reply.text = "Hello, #{message.from.first_name}!"
+    else
+      reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
     end
+    logger.info "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
   end
 end
