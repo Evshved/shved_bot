2ac69cf06a44a701c2a9e8100492a50c2d49d02d
Shved <snuggleshved@gmail.com>
2016-01-04 14:53:49 +0300

    First commit

--- app/controllers/telegram.rb
+++ app/controllers/telegram.rb
@@ -1,7 +1,3 @@
-
-
-
-
 require 'telegram/bot'
 
 token = '149230034:AAH_Q-s-KOxWaafJwlOMDWYQLxDMHNCehVU'
@@ -10,7 +6,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id,  text: "Kallalask **asdasd** llalal 2-2-2", disable_web_page_preview: true, parse_mode: *bold text*)
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
