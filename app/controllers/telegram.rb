8f8b892ebad64f10a7a256cc32b178552a4c7fe4
Shved <snuggleshved@gmail.com>
2016-01-04 15:46:01 +0300

    Solve Markdown

--- app/controllers/telegram.rb
+++ app/controllers/telegram.rb
@@ -6,9 +6,18 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id,  text: "Kallalask **asdasd** llalal 2-2-2", disable_web_page_preview: true, parse_mode: *bold text*)
+      bot.api.send_message(chat_id: message.chat.id,
+      text: "Hello, *#{message.from.first_name}*!",
+      disable_web_page_preview: false, parse_mode: 'Markdown')
+      bot.api.send_message(chat_id: message.chat.id,
+      text: "[Visit my documentation.](https://github.com/Evshved/shved_bot)",
+      disable_web_page_preview: false, parse_mode: 'Markdown')
+      bot.api.send_message(chat_id: message.chat.id,
+      text: "Type  _help_ for more info.",
+      disable_web_page_preview: false, parse_mode: 'Markdown')
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id,
+      text: "Bye, #{message.from.first_name}")
     end
   end
 end
